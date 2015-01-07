24826ca77a13428c899d03e0d1abe59f63d4ebbe
Ryan Dunckel <sparty02@gmail.com>
2015-01-06 21:54:59 -0600

    Rough implementation of all endpoints and draft test case

--- test/redbox-api_test.js
+++ test/redbox-api_test.js
@@ -1,12 +1,34 @@
 'use strict';
 
-var redboxApi = require('../');
+var redbox = require('../lib/redbox-api');
 var assert = require('should');
 
-describe('redboxApi', function () {
+describe('redbox', function () {
 
-  it('should be awesome', function () {
-    redboxApi().should.equal('awesome');
+  // it('should be awesome', function () {
+  //   redboxApi().should.equal('awesome');
+  // });
+
+  var apiKey = '12345';
+
+  it('should return games', function () {
+    redbox(apiKey).games.all().then(console.log);
+  });
+
+  it('should return inventory', function () {
+    redbox(apiKey).inventory.byPostalCode(53051).then(console.log);
+  });
+
+  it('should return movies', function () {
+    redbox(apiKey).movies.all().then(console.log);
+  });
+
+  it('should return products', function () {
+    redbox(apiKey).products.all().then(console.log);
+  });
+
+  it('should return stores', function () {
+    redbox(apiKey).stores.byPostalCode(53051).then(console.log).catch(console.log);
   });
 
 });
